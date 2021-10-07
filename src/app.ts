8b7dc64e74354e66a7a97d1b9ac1661ceb4976b5
Haf <haf@protonmail.ch>
2021-10-07 19:45:29 +0200

    rendering form

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,48 @@
 class AppContainer {
   private readonly mainDiv = document.getElementById("app")! as HTMLDivElement;
 
-  public append(element: HTMLElement) {
+  public prepend(element: HTMLElement) {
     this.mainDiv.insertAdjacentElement("afterbegin", element);
   }
 }
 
-function createForm(): HTMLFormElement {
-  const template = document.getElementById("project-input")! as HTMLTemplateElement;
-  const importedNode = document.importNode(template.content, true);
-  const form = importedNode.firstElementChild as HTMLFormElement;
-  form.id = "user-input";
+class UserInput {
+  private form;
+  private titleInput;
+  private descriptionInput;
+  private peopleInput;
 
-  return form;
+  constructor(appContainer: AppContainer) {
+    this.form = this.render(appContainer);
+
+    this.titleInput = document.getElementById("title")! as HTMLInputElement;
+    this.descriptionInput = document.getElementById("description")! as HTMLInputElement;
+    this.peopleInput = document.getElementById("people")! as HTMLInputElement;
+
+    this.listen();
+  }
+
+  private render(appContainer: AppContainer) {
+    const template = document.getElementById("project-input")! as HTMLTemplateElement;
+    const importedNode = document.importNode(template.content, true);
+    const form = importedNode.firstElementChild as HTMLFormElement;
+    form.id = "user-input";
+
+    appContainer.prepend(form);
+
+    return form;
+  }
+
+  private listen() {
+    this.form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      console.log("Tried to submit!");
+
+    });
+  }
 }
 
-const appContainer = new AppContainer();
 
-appContainer.append(createForm());
+const appContainer = new AppContainer();
+const userInput = new UserInput(appContainer);
+
