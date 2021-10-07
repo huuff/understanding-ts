effc23ccfa94e37f855f82362b073b99b76296ba
Haf <haf@protonmail.ch>
2021-10-07 19:32:28 +0200

    starting out

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-// Code goes here!
+class AppContainer {
+  private readonly mainDiv = document.getElementById("app")! as HTMLDivElement;
+
+  public append(element: HTMLElement) {
+    this.mainDiv.insertAdjacentElement("afterbegin", element);
+  }
+}
+
+function createForm(): HTMLFormElement {
+  const template = document.getElementById("project-input")! as HTMLTemplateElement;
+  const importedNode = document.importNode(template.content, true);
+  const form = importedNode.firstElementChild as HTMLFormElement;
+  form.id = "user-input";
+
+  return form;
+}
+
+const appContainer = new AppContainer();
+
+appContainer.append(createForm());
