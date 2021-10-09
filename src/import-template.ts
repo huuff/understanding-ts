32335adef533fcda2bf24f50a263df0cfb56de6c
Haf <haf@protonmail.ch>
2021-10-09 13:41:02 +0200

    remove unused

--- src/import-template.ts
+++
@@ -1,8 +0,0 @@
-export function importTemplate<T extends HTMLElement>(id: string, withId: string): T {
-  const template = document.getElementById(id)! as HTMLTemplateElement;
-  const importedNode = document.importNode(template.content, true);
-  const resultElement = importedNode.firstElementChild as T;
-  resultElement.id = withId;
-
-  return resultElement;
-}
