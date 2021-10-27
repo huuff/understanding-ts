693e6eabbf9a29790e6cd67acc19fdec65cdb623
Haf <haf@protonmail.ch>
2021-10-27 21:08:26 +0200

    just setting up webpack

---
+++ dnd_v2/webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
+
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    historyApiFallback: true,
+    hot: true
+  },
+
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html'
+  })],
+};
