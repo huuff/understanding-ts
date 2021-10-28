9acfa0e260188adf1d5950f8048f64b094527e14
Haf <haf@protonmail.ch>
2021-10-28 19:29:07 +0200

    showing a list header

--- dnd_v2/src/index.ts
+++ dnd_v2/src/index.ts
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form } from "./form";
 import { App } from "./app";
 
--- dnd_v2/src/project-list.ts
+++ dnd_v2/src/project-list.ts
@@ -10,11 +10,21 @@ export class ProjectList {
     private readonly projectStatus: ProjectStatus
   ) {
     this.component = new Component("projectListsLocation", "projectListTemplate", `${projectStatus}List`);
-    this.component.render(x => x);
+    this.component.render(this.addHeader.bind(this));
     console.log(`Created a list for ${projectStatus} projects`)
   }
 
   public get id() {
     return this.component.element.id;
+  }
+
+  private addHeader(elem: HTMLUListElement): HTMLUListElement {
+    const listHeader = elem.firstElementChild! as HTMLLIElement;
+    listHeader.innerText = `${this.titleCase(this.projectStatus)} projects`;
+    return elem;
+  }
+
+  private titleCase(string: string): string {
+    return string[0].toUpperCase() + string.substr(1);
   }
 }
